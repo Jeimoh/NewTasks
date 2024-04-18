19318be5d045de568ea9d85d135fc23fd8e345cf
Jeimoh <wjeimoh@gmail.com>
2024-04-19 00:31:39 +0300

    Great MOve Tommorrow am diving in Databases

--- TasksProject/Tasks/views.py
+++ TasksProject/Tasks/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
+now = datetime.datetime.now()
+date_now = now.date()
 
+
+tasks = [
+    {'id': 1, 'name': 'Wake up', 'Date': date_now},
+    {'id': 2, 'name': 'Take a strong Breakfast', 'Date': date_now},
+    {'id': 3, 'name': 'Go for a morning Run', 'Date': date_now},
+    {'id': 4, 'name': 'Code', 'Date': date_now},
+]
 def home(request):
-    return render(request, 'Tasks/index.html')
+    context = {'tasks': tasks}
+    return render(request, 'Tasks/index.html',context)
+
+def task(request,pk):
+    task = None
+    for i in tasks:
+        if i['id'] == int(pk):
+            task = i
+    context = {'task': task}
+    return render(request, 'Tasks/task.html',context)
